7db9d6a8ced458bebdd53a26c2cdccb2d42b6a15
iammanishanand <manishanand869@gmail.com>
2025-02-17 11:17:00 +0530

    updated the models in polls app

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
+# Represents a single question in the poll booth
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)  # The question itself
-    pub_date = models.DateTimeField("date published") # When the question was made
+    question_text = models.CharField(max_length=200)  # Question text
+    pub_date = models.DateTimeField('date published')  # Publication date
 
+    def __str__(self):
+        return self.question_text
+
+# Represents a choice for a given question in the poll booth
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)    # Answer option
-    votes = models.IntegerField(default=0)            # Vote tally
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)  # Link to the Question
+    choice_text = models.CharField(max_length=200)  # Choice text
+    votes = models.IntegerField(default=0)  # Vote count
+
+    def __str__(self):
+        return self.choice_text
