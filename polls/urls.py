ef17aaaedebb167fbc73671416ca2c1495174450
iammanishanand <manishanand869@gmail.com>
2025-02-17 11:30:59 +0530

    updated urls  in polls app

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'polls'  # Naming your booth
-
+app_name = "polls"
 urlpatterns = [
-    path('', views.index, name='index'),  # The entrance of your booth
-    path('<int:question_id>/', views.detail, name='detail'),  # Detailed question page
-    path('<int:question_id>/results/', views.results, name='results'),  # Results section
-    path('<int:question_id>/vote/', views.vote, name='vote'),  # Voting area
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
