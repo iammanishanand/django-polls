3cc60a11d1e4bfdbfdcf8e59fc222a18f24555a5
iammanishanand <manishanand869@gmail.com>
2025-02-17 11:28:28 +0530

    updated the views  in polls app

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,25 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Choice, Question
 
-def index(request):  # Staff showing available polls
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-
-def detail(request, question_id):  # Staff explaining a poll
+def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):  # Staff showing results
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
-def vote(request, question_id):  # Staff taking votes
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/vote.html', {'question': question})
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # If no choice is selected, show an error message.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1  # Increment the vote count.
+        selected_choice.save()
+        # Redirect the user to the results page.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
