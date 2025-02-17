40bd63d9c47b6fbd6ed9ef6d51ee778e076190b5
iammanishanand <manishanand869@gmail.com>
2025-02-17 12:01:06 +0530

    check the new test for views ui

--- polls/tests.py
+++ polls/tests.py
@@ -1,31 +1,34 @@
-import datetime
-
 from django.test import TestCase
+from django.urls import reverse
+from .models import Question
 from django.utils import timezone
 
-from .models import Question
+class PollsViewTests(TestCase):
 
-class QuestionModelTests(TestCase):
-    def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently() should return False for questions whose pub_date is in the future.
-        """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
+    def setUp(self):
+        # Creating a sample question for tests
+        self.question = Question.objects.create(
+            question_text="Sample Question?",
+            pub_date=timezone.now() - timezone.timedelta(days=1)
+        )
 
-    def test_was_published_recently_with_old_question(self):
-        """
-        was_published_recently() should return False for questions older than 1 day.
-        """
-        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-        old_question = Question(pub_date=time)
-        self.assertIs(old_question.was_published_recently(), False)
+    def test_home_page(self):
+        """Test that the home page shows the correct data."""
+        response = self.client.get(reverse('polls:index'))  # Simulate a GET request to the home page
+        self.assertEqual(response.status_code, 200)  # Check that the status code is 200 (OK)
+        self.assertTemplateUsed(response, 'polls/index.html')  # Ensure the correct template is used
+        self.assertContains(response, "Sample Question?")  # Verify if the question text is in the response
 
-    def test_was_published_recently_with_recent_question(self):
-        """
-        was_published_recently() should return True for questions published within the last day.
-        """
-        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-        recent_question = Question(pub_date=time)
-        self.assertIs(recent_question.was_published_recently(), True)
+    def test_detail_view(self):
+        """Test that the detail view works for a given question."""
+        url = reverse('polls:detail', args=(self.question.id,))  # Reverse generates the URL for the detail page
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)  # Check that the status code is 200 (OK)
+        self.assertTemplateUsed(response, 'polls/detail.html')  # Ensure the correct template is used
+        self.assertContains(response, "Sample Question?")  # Ensure the question text appears
+
+    def test_detail_view_with_no_question(self):
+        """Test the detail view for a non-existent question."""
+        url = reverse('polls:detail', args=(999,))  # Using an invalid question ID (999)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)  # Ensure we get a 404 status code for an invalid question
